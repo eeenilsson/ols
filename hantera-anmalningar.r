bce2a73f02f3f43ee4819749d1ee92891a29d01e
enilsson <eeenilsson@hotmail.com>
2018-09-18 17:43:27 +0200

    dodshjalp bild

--- hantera-anmalningar.r
+++ hantera-anmalningar.r
@@ -32,41 +32,82 @@ add_email <- c("ulrica.thunberg@regionorebrolan.se",
                "eva.melander@regionorebrolan.se",
                "anna.winberg@regionorebrolan.se",
                "lena.wijk@regionorebrolan.se",
-               "ulrica.thunberg@regionorebrolan.se")
+               "ulrica.thunberg@regionorebrolan.se",
+               "timmisen@gmail.com",
+               "faduma.omar@regionorebrolan.se",
+               "anna.olivecrona@regionorebrolan.se",
+               "viktor.olofsson@regionorebrolan.se",
+               "magnus.svensson@regionorebrolan.se",
+               "gina.sidiqi@regionorebrolan.se",
+               "gabriella.widlund@regionorebrolan.se",
+               "teres.litzell@regionorebrolan.se",
+               "eric.stenninger@regionorebrolan.se",
+               "erik.schwarcz@regionorebrolan.se",
+               "john.henriksson@regionorebrolan.se"
+               )
 
-## Avbokat: charlotta.wrenninge@regionorebrolan.se, asa ludvigsson
+## EJ FIKA
+ej_fika <- c("erik.schwarcz@regionorebrolan.se",
+             "john.henriksson@regionorebrolan.se"
+             )
+
+## Avbokat:
+avbokat <- c("charlotta.wrenninge@regionorebrolan.se", "emma.wiberg@regionorebrolan.se", "bo.soderquist@regionorebrolan.se", "alden_jenny@hotmail.com", "asa.ludvigsson@regionorebrolan.se")
+##, asa ludvigsson,
 ## 6 extra frallor
 
 #### corrections
-email_list <- c(anm_sjvdb["Email.address"][[1]], add_email)
-email_list <- gsub("ggunnar", "gunnar", email_list)
-email_list <- gsub("maud_carpenter@hotmail.co", "maud_carpenter@hotmail.com", email_list)
-### write email list to dropbox
+anm_sjvdb["Email.address"][[1]] <- gsub("ggunnar", "gunnar", anm_sjvdb["Email.address"][[1]])
+anm_sjvdb["Email.address"][[1]] <- gsub("maud_carpenter@hotmail.co", "maud_carpenter@hotmail.com", anm_sjvdb["Email.address"][[1]])
+
+## lista unika epostadresser minus avbokade
+anm <- c(
+    unique(anm_sjvdb["Email.address"][[1]]),
+  add_email) ## unika anmälda (en del kan ha avbokat)
+anm <- anm[!(anm %in% avbokat)] ## minus avbokade
+anm <- unique(anm) ## remove duplicated
+
+## email list fralla = JA
+anm_sjvdb%>%
+    filter(Önskar.du.fralla...dryck.före.debatten. == "JA tack") -> temp
+fralla <-  c(temp$Email.address, add_email)
+fralla <- fralla[fralla %in% anm] ## minus avbokade
+fralla <- fralla[!duplicated(fralla)]
+fralla <- fralla[!(fralla %in% ej_fika)] ## minu ej fika
+utan_fralla <- anm[!(anm %in% fralla)]
+
+## check
+c(fralla, utan_fralla) %in% anm
+length(fralla) + length(utan_fralla) ==
+    length(anm)
+
+## write email lists
 setwd(wd_dropbox)
-
 write_lines(
-    paste(anm_sjvdb$Email.address, ",", sep = "")
+    paste(anm, ",", sep = "")
   , path = paste(wd_dropbox, "anm_sjukvardsdebatt_epostlista.txt", sep=""))
-
-setwd(wd)
+write_lines(
+    paste(fralla, ",", sep = "")
+  , path = paste(wd_dropbox, "anm_sjukvardsdebatt_fralla.txt", sep=""))
+write_lines(
+    paste(utan_fralla, ",", sep = "")
+  , path = paste(wd_dropbox, "anm_sjukvardsdebatt_utan_fralla.txt", sep=""))
 
 ## print anmalda
-unique(anm_sjvdb["Email.address"][[1]]) ## unika anmälda
-length(unique(anm_sjvdb["Email.address"][[1]])) ## antal anmälda
-sum(anm_sjvdb[["Önskar.du.fralla...dryck.före.debatten."]] == "JA tack") ## antal frallor
+anm
+length(anm) ## antal anmälda
+length(fralla)
+length(add_email)
+length(avbokat)
 anm_sjvdb[["Kostrestriktioner..standardfrallan.är.vegetarisk."]][!is.na(anm_sjvdb[["Kostrestriktioner..standardfrallan.är.vegetarisk."]])]
 
-## 159 unika anmälningar, varav 142 önskar fralla.
+names(anm_sjvdb)
+
+## 159 unika anmälningar, varav 153 önskar fralla.
 ## Glutenfritt 3 st
 ## Gluten- och laktosfritt 1 st
 ## Laktosfritt 2 st
 ## Vegan 2 st
-
-## Sena anmälningar som tillkommer:
-## ulrica.thunberg@regionorebrolan.se
-
-## Avanmälningar
-## Maja Wästfelt (Kvittas mot Ulrica ovan)
 
 ## uppdatera medlemsregister
 setwd(wd_dropbox)
